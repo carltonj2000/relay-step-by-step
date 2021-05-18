4dd69bf6c995c38efe5434486d8ce1c73ba7963a
Carlton Joseph <carlton.joseph@gmail.com>
2021-05-18 15:01:01 -0700

    add relay

--- src/App.js
+++ src/App.js
@@ -1,45 +1,47 @@
 import React from "react";
 import "./App.css";
-import fetchGraphQL from "./fetchGraphQL";
+import graphql from "babel-plugin-relay/macro";
+import {
+  RelayEnvironmentProvider,
+  loadQuery,
+  usePreloadedQuery,
+} from "react-relay/hooks";
+import RelayEnvironment from "./RelayEnvironment";
 
-const { useState, useEffect } = React;
+const { Suspense } = React;
 
-function App() {
-  const [name, setName] = useState(null);
+const RepositoryNameQuery = graphql`
+  query AppRepositoryNameQuery {
+    repository(owner: "facebook", name: "relay") {
+      name
+    }
+  }
+`;
 
-  useEffect(() => {
-    let isMounted = true;
-    fetchGraphQL(`
-      query RepositoryNameQuery {
-        repository(owner: "facebook" name: "relay") {
-          name
-        }
-      }
-    `)
-      .then((response) => {
-        if (!isMounted) {
-          return;
-        }
-        const data = response.data;
-        setName(data.repository.name);
-        console.log(data);
-      })
-      .catch((error) => {
-        console.error(error);
-      });
+const preloadedQuery = loadQuery(RelayEnvironment, RepositoryNameQuery, {
+  /* query variables */
+});
 
-    return () => {
-      isMounted = false;
-    };
-  }, []);
+function App(props) {
+  const data = usePreloadedQuery(RepositoryNameQuery, props.preloadedQuery);
 
   return (
     <div className="App">
       <header className="App-header">
-        <p>{name != null ? `Repository: ${name}` : "Loading"}</p>
+        <p>{data.repository.name}</p>
       </header>
     </div>
   );
 }
 
-export default App;
+function AppRoot(props) {
+  return (
+    <RelayEnvironmentProvider environment={RelayEnvironment}>
+      <Suspense fallback={"Loading..."}>
+        <App preloadedQuery={preloadedQuery} />
+      </Suspense>
+    </RelayEnvironmentProvider>
+  );
+}
+
+export default AppRoot;
